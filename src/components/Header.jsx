af98fc67a098e7c539b6a055554b65848ff6b1e8
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-12-10 22:39:35 +0530

    categories ui & fetch categories from giphy api

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { HiEllipsisVertical, HiMiniBars3BottomRight } from "react-icons/hi2";
+import { GifState } from "../context/gifContext";
 
 const Header = () => {
   const [categories, setCategories] = useState([]);
   const [showCategories, setShowCategories] = useState(false);
+  const { gif, filter, setFilter, favorites } = GifState();
+
+  const fetchGifCategories = async () => {
+    const { data } = await gif.categories();
+    setCategories(data);
+  };
+  useEffect(() => {
+    fetchGifCategories();
+  }, []);
   return (
     <nav>
       <div className="relative flex gap-4 justify-between items-center mb-2">
@@ -16,9 +26,19 @@ const Header = () => {
         </Link>
 
         <div className="font-bold text-md flex gap-2 items-center">
-          <Link className="px-4 py-1 hover:gradient border-b-4 hidden lg:block">
-            Reactions{" "}
-          </Link>
+          {/* render categories */}
+          {categories.slice(0, 5)?.map((category) => {
+            return (
+              <Link
+                key={category.name}
+                to={`/${category.name_encoded}`}
+                className="px-4 py-1 hover:gradient border-b-4 hidden lg:block"
+              >
+                {category.name}
+              </Link>
+            );
+          })}
+
           <button onClick={() => setShowCategories(!showCategories)}>
             <HiEllipsisVertical
               size={35}
@@ -27,9 +47,11 @@ const Header = () => {
               } border-b-4 hidden lg:block`}
             />
           </button>
-          <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
-            <Link to="/favorites">Favorite GIFs</Link>
-          </div>
+          {favorites.lenght > 0 && (
+            <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
+              <Link to="/favorites">Favorite GIFs</Link>
+            </div>
+          )}
           <div>
             <button>
               <HiMiniBars3BottomRight
@@ -42,10 +64,20 @@ const Header = () => {
 
         {showCategories && (
           <div className="absolute right-0 top-14 px-10 pt-6 pb-9 w-full gradient z-20">
-            <span>Categories</span>
-            <hr />
-            <div>
-              <Link className="font-bold">Reactions</Link>
+            <span className="text-3xl font-extrabold">Categories</span>
+            <hr className="bg-gray-100 opacity-40 my-5" />
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 text-md">
+              {categories.map((category) => {
+                return (
+                  <Link
+                    key={category.name}
+                    to={`/${category.name_encoded}`}
+                    className="font-bold"
+                  >
+                    {category.name}
+                  </Link>
+                );
+              })}
             </div>
           </div>
         )}
--- src/context/GifContext.jsx
+++ src/context/GifContext.jsx
@@ -1,9 +1,24 @@
-import { createContext } from "react";
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import { createContext, useContext, useState } from "react";
 
 const GifContext = createContext();
 
 const GifProvider = ({ children }) => {
-  return <GifContext.Provider>{children}</GifContext.Provider>;
+  const [gifs, setGifs] = useState([]);
+  const [filter, setFilter] = useState("gifs");
+  const [favorites, setFavorites] = useState([]);
+
+  const gif = new GiphyFetch(import.meta.env.VITE_GIPHY_API);
+  return (
+    <GifContext.Provider
+      value={{ gif, gifs, setGifs, filter, setFilter, favorites }}
+    >
+      {children}
+    </GifContext.Provider>
+  );
 };
 
+export const GifState = () => {
+  return useContext(GifContext);
+};
 export default GifProvider;
