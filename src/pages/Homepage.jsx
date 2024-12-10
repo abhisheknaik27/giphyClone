24c7cdbcee5e606ca813003f43311372db3a024d
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-12-10 23:01:34 +0530

    gif homepage (trending gif fetched)

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { GifState } from "../context/gifContext";
+import Gif from "../components/Gif";
 
 const Homepage = () => {
-  return <div>home</div>;
+  const { gif, gifs, setGifs, filter } = GifState();
+  const fetchTrendingGifs = async () => {
+    const { data } = await gif.trending({
+      limit: 20,
+      type: filter,
+      rating: "g",
+    });
+    setGifs(data);
+    console.log(gifs);
+  };
+  useEffect(() => {
+    fetchTrendingGifs();
+  }, [filter]);
+  return (
+    <div>
+      <img src="/banner.gif" alt="banner" className="mt-4 rounded w-full" />
+      {/* <FilterGif /> */}
+
+      <div className="columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-2 mt-2">
+        {gifs.map((gif) => {
+          return <Gif gif={gif} key={gif.title} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
