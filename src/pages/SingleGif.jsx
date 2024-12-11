e2893b55a8ea2ad39e14a9785c10b71075142ae0
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-12-11 11:28:17 +0530

    single page ui & related gifs

--- src/pages/SingleGif.jsx
+++ src/pages/SingleGif.jsx
@@ -2,7 +2,15 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { GifState } from "../context/GifContext";
 import Gif from "../components/Gif";
-import { HiMiniChevronDown, HiMiniChevronUp } from "react-icons/hi2";
+import {
+  HiMiniChevronDown,
+  HiMiniChevronUp,
+  HiMiniHeart,
+} from "react-icons/hi2";
+import FollowOn from "../components/FollowOn";
+import { HiOutlineExternalLink } from "react-icons/hi";
+import { FaPaperPlane } from "react-icons/fa";
+import { IoCodeSharp } from "react-icons/io5";
 
 const contentType = ["gifs", "stickers", "text"];
 
@@ -12,7 +20,10 @@ const SingleGif = () => {
   const [relatedGifs, setRelatedGifs] = useState([]);
   const [readMore, setReadMore] = useState(false);
 
-  const { gif } = GifState();
+  const { gif, addToFavorites, favorites } = GifState();
+
+  const shareGif = () => {};
+  const embedGif = () => {};
 
   const fetchGif = async () => {
     const gifId = slug.split("-");
@@ -50,13 +61,13 @@ const SingleGif = () => {
               <p className="py-4 whitespace-pre-line text-sm text-gray-400">
                 {readMore
                   ? singleGif?.user?.description
-                  : singleGif?.user?.description.slice(0, 100)}
-                {readMore.length > 100 && (
+                  : singleGif?.user?.description.slice(0, 100) + "..."}
+                {singleGif?.user?.description.length > 100 && (
                   <div
                     className="flex items-center faded-text cursor-pointer"
                     onClick={() => setReadMore(!readMore)}
                   >
-                    {readMore ? (
+                    {!readMore ? (
                       <>
                         Read Less <HiMiniChevronUp size={20} />
                       </>
@@ -71,6 +82,19 @@ const SingleGif = () => {
             )}
           </>
         )}
+        <FollowOn />
+        <div className="divider" />
+        {singleGif?.source && (
+          <div>
+            <span className="faded-text">Source</span>
+            <div className="flex items-center text-sm font-bold gap-1">
+              <HiOutlineExternalLink size={25} />
+              <a href={singleGif.source} target="_blank" className="truncate">
+                {singleGif.source}
+              </a>
+            </div>
+          </div>
+        )}
       </div>
       <div className="col-span-4 sm:col-span-3">
         <div className="flex gap-6">
@@ -80,12 +104,60 @@ const SingleGif = () => {
             </div>
             <Gif gif={singleGif} hover={false} />
             {/* mobile ui */}
+            <div className="flex sm:hidden gap-1">
+              <img
+                src={singleGif?.user?.avatar_url}
+                alt={singleGif?.user?.display_name}
+                className="h-14"
+              />
+              <div className="px-2">
+                <div className="font-bold">{singleGif?.user?.display_name}</div>
+                <div className="faded-text">@{singleGif?.user?.username}</div>
+              </div>
+
+              <button className="ml-auto" onClick={shareGif}>
+                <FaPaperPlane size={25} />
+              </button>
+            </div>
           </div>
-          favorites icon
+
+          <div className="hidden sm:flex flex-col gap-5 mt-6">
+            <button
+              onClick={() => addToFavorites(gif.id)}
+              className="flex gap-5 items-center font-bold text-lg"
+            >
+              <HiMiniHeart
+                size={30}
+                className={`${
+                  favorites.includes(gif.id) ? "text-rose-600" : ""
+                }`}
+              />
+              Favorite
+            </button>
+            <button
+              onClick={shareGif}
+              className="flex gap-6 items-center font-bold text-lg"
+            >
+              <FaPaperPlane size={25} />
+              Share
+            </button>
+            <button
+              onClick={embedGif}
+              className="flex gap-5 items-center font-bold text-lg"
+            >
+              <IoCodeSharp size={30} />
+              Embed
+            </button>
+          </div>
         </div>
         <div>
           {" "}
-          <span className="font-extrabold">Related Videos</span>
+          <span className="font-extrabold my-2">Related Videos</span>
+          <div className="columns-2 md:columns-3 gap-2">
+            {relatedGifs.slice(1).map((gif) => (
+              <Gif gif={gif} key={gif.id} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
